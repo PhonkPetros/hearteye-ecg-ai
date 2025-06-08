3bc05c037c2d33d54a9e398b2854b92a66569981
pierickm <113185406+pierickm@users.noreply.github.com>
2025-06-08 15:33:07 +0200

    Revert "fix refresh token bug"
    This reverts commit a5b9cb67820acd1f93b93b15a8808d32033cf816.

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, create_refresh_token
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, create_refresh_token, jwt_refresh_token_required
 from ..models import db, User
 
 auth_bp = Blueprint('auth', __name__)
